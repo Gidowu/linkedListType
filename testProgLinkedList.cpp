8a86063282d2c37338684a6720ef215568749f41
Edudzi-cloud <idowu1@kenyon.edu>
2024-10-20 20:29:01 -0400

    New changes made

--- testProgLinkedList.cpp
+++ testProgLinkedList.cpp
@@ -1,4 +1,4 @@
-//This program tests various operation of a linked list
+// This program tests various operations of an unordered linked list
 //34 62 21 90 66 53 88 24 10 -999
 
 #include <iostream>                                 //Line 1
@@ -6,63 +6,84 @@
 
 using namespace std;                                //Line 3
 
-int main()                                          //Line 4
-{                                                   //Line 5
-    unorderedLinkedList<int> list1, list2;          //Line 6
-    int num;                                        //Line 7
+int main() {                                        //Line 4
+    unorderedLinkedList<int> list1, list2;          //Line 5
+    int num;                                        //Line 6
 
-    cout << "Line 8: Enter integers ending "
-         << "with -999" << endl;                    //Line 8
-    cin >> num;                                     //Line 9
+    // Insert elements into list1
+    cout << "Enter integers ending with -999: ";    //Line 7
+    cin >> num;                                     //Line 8
 
-    while (num != -999)                             //Line 10
-    {                                               //Line 11
-        list1.insertLast(num);                      //Line 12
-        cin >> num;                                 //Line 13
-    }                                               //Line 14
+    while (num != -999) {                           //Line 9
+        list1.insertLast(num);                      //Line 10
+        cin >> num;                                 //Line 11
+    }                                               //Line 12
 
-    cout << endl;                                   //Line 15
+    // Print list1
+    cout << "\nlist1: ";                            //Line 13
+    list1.print();                                  //Line 14
+    cout << "\nLength of list1: " << list1.length() << endl;  //Line 15
 
-    cout << "Line 16: list1: ";                     //Line 16
-    list1.print();                                  //Line 17
-    cout << endl;                                   //Line 18
-    cout << "Line 19: Length of list1: "
-         << list1.length() << endl;                 //Line 19
+    // Test the assignment operator by assigning list1 to list2
+    list2 = list1;                                  //Line 16
 
-    list2 = list1;	   //test the assignment operator Line 20
+    cout << "\nlist2 (after assignment): ";         //Line 17
+    list2.print();                                  //Line 18
+    cout << "\nLength of list2: " << list2.length() << endl;  //Line 19
 
-    cout << "Line 21: list2: ";                     //Line 21
-    list2.print();                                  //Line 22
-    cout << endl;                                   //Line 23
-    cout << "Line 24: Length of list2: "
-         << list2.length() << endl;                 //Line 24
+    // Test deleteNode
+    cout << "\nEnter the number to be deleted from list2: ";  //Line 20
+    cin >> num;                                     //Line 21
+    list2.deleteNode(num);                          //Line 22
+    cout << "\nAfter deleting " << num << " from list2: ";    //Line 23
+    list2.print();                                  //Line 24
+    cout << "\nLength of list2: " << list2.length() << endl;  //Line 25
 
-    cout << "Line 25: Enter the number to be "
-         << "deleted: ";                            //Line 25
-    cin >> num;                                     //Line 26
-    cout << endl;                                   //Line 27
+    // Test deleteSmallest
+    cout << "\nTesting deleteSmallest() on list2..." << endl;  //Line 26
+    list2.deleteSmallest();                         //Line 27
+    cout << "list2 after deleting the smallest element: ";     //Line 28
+    list2.print();                                  //Line 29
+    cout << "\nLength of list2: " << list2.length() << endl;   //Line 30
 
-    list2.deleteNode(num);                          //Line 28
+    // Test deleteAllOccurrences
+    cout << "\nEnter a number to delete all occurrences from list2: ";  //Line 31
+    cin >> num;                                     //Line 32
+    list2.deleteAllOccurrences(num);                //Line 33
+    cout << "\nlist2 after deleting all occurrences of " << num << ": ";  //Line 34
+    list2.print();                                  //Line 35
+    cout << "\nLength of list2: " << list2.length() << endl;   //Line 36
 
-    cout << "Line 29: After deleting " << num
-         << " list2: " << endl;                     //Line 29
-    list2.print();                                  //Line 30
-    cout << endl;                                   //Line 31
+    // Test getKthElement
+    cout << "\nEnter the position (k) of the element you want to retrieve from list2: ";  //Line 37
+    int k;                                          //Line 38
+    cin >> k;                                       //Line 39
+    int kthElement = list2.getKthElement(k);        //Line 40
+    if (kthElement != -1)                           //Line 41
+        cout << "The " << k << "th element is: " << kthElement << endl;  //Line 42
 
-    cout << "Line 32: Length of list2: "
-         << list2.length() << endl;                 //Line 32
+    // Test deleteKthElement
+    cout << "\nEnter the position (k) of the element you want to delete from list2: ";  //Line 43
+    cin >> k;                                       //Line 44
+    list2.deleteKthElement(k);                      //Line 45
+    cout << "\nlist2 after deleting the " << k << "th element: ";  //Line 46
+    list2.print();                                  //Line 47
+    cout << "\nLength of list2: " << list2.length() << endl;   //Line 48
 
-    cout << endl << "Line 33: Output list1 "
-         << "using an iterator" << endl;            //Line 33
+    // Test rotate
+    cout << "\nTesting rotate() on list2..." << endl;  //Line 49
+    list2.rotate();                                   //Line 50
+    cout << "list2 after rotation: ";                 //Line 51
+    list2.print();                                    //Line 52
+    cout << "\nLength of list2: " << list2.length() << endl;   //Line 53
 
-    linkedListIterator<int> it;                     //Line 34
+    // Output list1 using an iterator
+    cout << "\nOutput list1 using an iterator: ";      //Line 54
+    linkedListIterator<int> it;                        //Line 55
 
-    for (it = list1.begin(); it != list1.end();
-                             ++it)                  //Line 35
-        cout << *it << " ";                         //Line 36
-    cout << endl;                                   //Line 37
+    for (it = list1.begin(); it != list1.end(); ++it)  //Line 56
+        cout << *it << " ";                            //Line 57
+    cout << endl;                                      //Line 58
 
-    //system("pause");
-    return 0;                                       //Line 38
-}                                                   //Line 39
-
+    return 0;                                          //Line 59
+}                                                      //Line 60
--- unorderedLinkedList.h
+++ unorderedLinkedList.h
@@ -14,7 +14,6 @@
 using namespace std;
 
 template <class Type>
-//class unorderedLinkedList: public linkedListType<Type>
 class unorderedLinkedList: public linkedListType<int>
 {
 public:
@@ -28,7 +27,6 @@ public:
       //Postcondition: first points to the new list, newItem is
       //    inserted at the beginning of the list, last points to
       //    the last node, and count is incremented by 1.
-      //
 
     void insertLast(const Type& newItem);
       //Function to insert newItem at the end of the list.
@@ -42,12 +40,33 @@ public:
       //    is deleted from the list. first points to the first
       //    node, last points to the last node of the updated
       //    list, and count is decremented by 1.
+
+    // New functions added for lab requirements
+    void deleteSmallest();
+      //Function to find and delete the node with the smallest value.
+      //Postcondition: The smallest node is deleted from the list.
+
+    void deleteAllOccurrences(int value);
+      //Function to delete all occurrences of the given value.
+      //Postcondition: All nodes with the value are deleted.
+
+    int getKthElement(int k);
+      //Function to return the info of the kth element of the linked list.
+      //Postcondition: Returns the value of the kth element.
+
+    void deleteKthElement(int k);
+      //Function to delete the kth element of the linked list.
+      //Postcondition: The kth element is deleted if it exists.
+
+    void rotate();
+      //Function to rotate the list: remove the first node and move it to the end.
+      //Postcondition: The first node is moved to the end of the list.
 };
 
+// Existing member function implementations
 
 template <class Type>
-bool unorderedLinkedList<Type>::
-                   search(const Type& searchItem) const
+bool unorderedLinkedList<Type>::search(const Type& searchItem) const
 {
     nodeType<Type> *current; //pointer to traverse the list
     bool found = false;
@@ -55,7 +74,7 @@ bool unorderedLinkedList<Type>::
     current = first; //set current to point to the first
                      //node in the list
 
-    while (current != NULL && !found)    //search the list
+    while (current != nullptr && !found)    //search the list
         if (current->info == searchItem) //searchItem is found
             found = true;
         else
@@ -77,8 +96,8 @@ void unorderedLinkedList<Type>::insertFirst(const Type& newItem)
                                 //actual first node
     count++;                    //increment count
 
-    if (last == NULL)   //if the list was empty, newNode is also
-                        //the last node in the list
+    if (last == nullptr)   //if the list was empty, newNode is also
+                           //the last node in the list
         last = newNode;
 }//end insertFirst
 
@@ -90,11 +109,10 @@ void unorderedLinkedList<Type>::insertLast(const Type& newItem)
     newNode = new nodeType<Type>; //create the new node
 
     newNode->info = newItem;  //store the new item in the node
-    newNode->link = NULL;     //set the link field of newNode
-                              //to NULL
+    newNode->link = nullptr;  //set the link field of newNode to nullptr
 
-    if (first == NULL)  //if the list is empty, newNode is
-                        //both the first and last node
+    if (first == nullptr)  //if the list is empty, newNode is
+                           //both the first and last node
     {
         first = newNode;
         last = newNode;
@@ -109,7 +127,6 @@ void unorderedLinkedList<Type>::insertLast(const Type& newItem)
     }
 }//end insertLast
 
-
 template <class Type>
 void unorderedLinkedList<Type>::deleteNode(const Type& deleteItem)
 {
@@ -117,7 +134,7 @@ void unorderedLinkedList<Type>::deleteNode(const Type& deleteItem)
     nodeType<Type> *trailCurrent; //pointer just before current
     bool found;
 
-    if (first == NULL)    //Case 1; the list is empty.
+    if (first == nullptr)    //Case 1: the list is empty
         cout << "Cannot delete from an empty list."
              << endl;
     else
@@ -127,46 +144,204 @@ void unorderedLinkedList<Type>::deleteNode(const Type& deleteItem)
             current = first;
             first = first->link;
             count--;
-            if (first == NULL)    //the list has only one node
-                last = NULL;
+            if (first == nullptr)    //the list has only one node
+                last = nullptr;
             delete current;
         }
         else //search the list for the node with the given info
         {
             found = false;
-            trailCurrent = first;  //set trailCurrent to point
-                                   //to the first node
-            current = first->link; //set current to point to
-                                   //the second node
+            trailCurrent = first;  //set trailCurrent to point to the first node
+            current = first->link; //set current to point to the second node
 
-            while (current != NULL && !found)
+            while (current != nullptr && !found)
             {
                 if (current->info != deleteItem)
                 {
                     trailCurrent = current;
-                    current = current-> link;
+                    current = current->link;
                 }
                 else
                     found = true;
             }//end while
 
-            if (found) //Case 3; if found, delete the node
+            if (found) //Case 3: if found, delete the node
             {
                 trailCurrent->link = current->link;
                 count--;
 
-                if (last == current)   //node to be deleted
-                                       //was the last node
-                    last = trailCurrent; //update the value
-                                         //of last
+                if (last == current)   //node to be deleted was the last node
+                    last = trailCurrent; //update the value of last
                 delete current;  //delete the node from the list
             }
             else
-                cout << "The item to be deleted is not in "
-                     << "the list." << endl;
+                cout << "The item to be deleted is not in the list." << endl;
         }//end else
     }//end else
 }//end deleteNode
 
+// Add the new function implementations
+
+// Function to find and delete the node with the smallest value
+template <class Type>
+void unorderedLinkedList<Type>::deleteSmallest() {
+    if (first == nullptr) {  // Empty list case
+        cout << "The list is empty. No element to delete." << endl;
+        return;
+    }
+
+    nodeType<Type> *current = first;
+    nodeType<Type> *smallest = first;
+    nodeType<Type> *prev = nullptr;  // Track previous node of current
+    nodeType<Type> *smallestPrev = nullptr;  // Track previous node of smallest
+
+    // Traverse the list to find the smallest node
+    while (current != nullptr) {
+        if (current->info < smallest->info) {  // Update smallest node and its previous pointer
+            smallest = current;
+            smallestPrev = prev;
+        }
+        prev = current;
+        current = current->link;
+    }
+
+    // Now delete the smallest node
+    if (smallest == first) {  // Special case: smallest node is the first node
+        first = first->link;  // Update the first pointer to the next node
+        if (first == nullptr) {  // If the list becomes empty after deletion
+            last = nullptr;  // Update last to nullptr (empty list case)
+        }
+    } else {  // Smallest node is somewhere in the middle or end
+        smallestPrev->link = smallest->link;  // Bypass the smallest node
+        if (smallest == last) {  // If smallest node is the last one
+            last = smallestPrev;  // Update the last pointer
+        }
+    }
+
+    delete smallest;
+    count--;  // Decrease the node count
+
+    // If the list is empty after deletion, ensure count is set to 0
+    if (first == nullptr) {
+        count = 0;
+    }
+}
+
+
+template <class Type>
+void unorderedLinkedList<Type>::deleteAllOccurrences(int value) {
+    nodeType<Type> *current = first;
+    nodeType<Type> *prev = nullptr;
+
+    while (current != nullptr) {
+        if (current->info == value) {
+            if (prev == nullptr) {
+                // If the first node is being deleted
+                first = current->link;
+            } else {
+                prev->link = current->link;
+            }
+            // Check if the deleted node was the last node
+            if (current == last) {
+                last = prev;
+            }
+            nodeType<Type> *temp = current;
+            current = current->link;
+            delete temp;
+            count--;  // Decrease the count of nodes
+        } else {
+            prev = current;
+            current = current->link;
+        }
+    }
+
+    // If the list is empty after deletion, set count to 0
+    if (first == nullptr) {
+        last = nullptr;
+        count = 0;
+    }
+}
+
+
+
+
+// Function to return the kth element
+template <class Type>
+int unorderedLinkedList<Type>::getKthElement(int k) {
+    nodeType<Type> *current = first;
+    int count = 1;
+
+    while (current != nullptr && count < k) {
+        current = current->link;
+        count++;
+    }
+
+    if (current == nullptr) {
+        cout << "Element out of bounds!" << endl;
+        return -1;  // Return -1 if out of bounds
+    }
+
+    return current->info;
+}
+
+// Function to delete the kth element
+template <class Type>
+void unorderedLinkedList<Type>::deleteKthElement(int k) {
+    if (first == nullptr) return;  // Empty list case
+
+    nodeType<Type> *current = first;
+    nodeType<Type> *prev = nullptr;
+    int countPos = 1;  // Start position at 1
+
+    while (current != nullptr && countPos < k) {
+        prev = current;
+        current = current->link;
+        countPos++;
+    }
+
+    if (current == nullptr) {
+        cout << "Element out of bounds!" << endl;
+        return;
+    }
+
+    // Deleting the first node
+    if (prev == nullptr) {
+        first = current->link;
+    } else {
+        prev->link = current->link;
+    }
+
+    // If the node being deleted is the last one, update the last pointer
+    if (current == last) {
+        last = prev;
+    }
+
+    delete current;
+    count--;  // Decrease the count of nodes
+
+    // If the list is now empty, set count to 0
+    if (first == nullptr) {
+        last = nullptr;
+        count = 0;
+    }
+}
+
+
+// Function to rotate the list (move the first node to the end)
+template <class Type>
+void unorderedLinkedList<Type>::rotate() {
+    if (first == nullptr || first->link == nullptr) return;  // Empty list or single element case
+
+    nodeType<Type> *current = first;
+    nodeType<Type> *newFirst = first->link;
+
+    while (current->link != nullptr) {
+        current = current->link;
+    }
+
+    current->link = first;  // Move the first node to the end
+    first->link = nullptr;  // Unlink the old first node
+    first = newFirst;
+}
 
 #endif
